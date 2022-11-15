317249bea6c4f6ba790f2b5b42487112623a6246
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-11-15 14:47:57 +0100

    Update Program.cs

--- Get to the exit/Program.cs
+++ Get to the exit/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +11,31 @@ namespace Get_to_the_exit
         static void Main(string[] args)
         {
             drawning l1 = new drawning(1,1);
-            l1.draw_cube();
-            l1.draw_player();
+            while (true)
+            {
+                l1.draw_all();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.A:
+                        if (l1.isValid(-1, 0))
+                            l1.x_plyr--;
+                        break;
+                    case ConsoleKey.S:
+                        if (l1.isValid(0, -1))
+                            l1.y_plyr--;
+                        break;
+                    case ConsoleKey.D:
+                        if (l1.isValid(1, 0))
+                            l1.x_plyr++;
+                        break;
+                    case ConsoleKey.W:
+                        if (l1.isValid(0, 1))
+                            l1.x_plyr++;
+                        break;
+                    default:
+                        break;
+                }
+            }
             Console.ReadKey();
         }
     }
